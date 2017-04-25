dcb456a1fe82579d86db36bff9c8f8c2c5d66590
Michael Ulrich <michaeldulrich@gmail.com>
2017-04-25 08:15:07 -0400

    Added access controller and login page

---
+++ app/controllers/access_controller.rb
@@ -0,0 +1,41 @@
+class AccessController < ApplicationController
+
+  def login
+    # login form /views/access/login
+  end
+
+  def attempt_login
+    if params[:username].present? && params[:password].present?
+      found_user = User.where(:username => params[:username]).first
+      # could use an else to put a message into flash for "please input username and password."
+      if found_user
+        authorized_user = found_user.authenticate(params[:password])
+      end
+    end
+
+    if authorized_user
+      session[:user_id] = authorized_user.id
+      session[:username] = authorized_user.username
+      flash[:notice] = "You are now logged in."
+      redirect_to(user_path)
+    else
+      flash.now[:notice] = "Invalid username/password combination."
+      render('login')
+    end
+
+  end
+
+  def logout
+
+  end
+
+  private
+
+  def confirm_logged_in
+    unless session[:user_id]
+      flash[:notice] = "Please log in."
+      redirect_to(access_login_path)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,9 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'access/login'
+  post 'access/attempt_login'
+  get 'access/logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
