5c052094d069f1f241ceec4b50751e728efd95c6
Michael Ulrich <michaeldulrich@gmail.com>
2017-05-24 09:14:20 -0400

    Image model and controller
    Uploading now works for gifs.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,14 +5,44 @@ class ImagesController < AccessController
   end
 
   def show
+    @image = Image.find(params[:id])
   end
 
   def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      flash[:notice] = "You saved a new image! Great work."
+      redirect_to(images_path)
+    else
+      render('new')
+    end
   end
 
   def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+
   end
 
   def delete
+    @image = Image.find(params[:id])
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    redirect_to(images_path)
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:upload, :name)
   end
 end
--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -51,7 +51,7 @@ class MemesController < AccessController
   private
 
   def meme_params
-    params.require(:meme).permit(:name, :content_type, :content, :image)
+    params.require(:meme).permit(:name, :content_type, :content)
   end
 
 end
